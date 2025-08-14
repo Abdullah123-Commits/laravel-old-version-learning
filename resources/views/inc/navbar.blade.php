460106dd517f0945434bcca2a7495ff5b57ae910
Abdullah123-Commits <abdullahmuaz002@gmail.com>
2025-08-14 12:56:42 +0500

    add resource controller named PostsController

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -12,9 +12,9 @@
 
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/services">Services</a></li>
+                <li><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ url('/about') }}">About</a></li>
+                <li><a href="{{ url('/services') }}">Services</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
